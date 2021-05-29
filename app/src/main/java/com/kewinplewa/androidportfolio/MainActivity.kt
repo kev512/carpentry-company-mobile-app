ff718652d539b35824694d76d6c68eb834236cf5
kev512 <kewin5@onet.eu>
2021-05-29 22:32:44 +0200

    New Activity - Splash Screen

--- app/src/main/java/com/kewinplewa/androidportfolio/MainActivity.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kewinplewa.androidportfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Witaj!", Toast.LENGTH_SHORT).show()
+
     }
 }
