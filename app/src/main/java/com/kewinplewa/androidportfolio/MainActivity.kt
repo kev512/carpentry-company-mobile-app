b48f7c64b9c351305ada8b6c15a5ad6e48735949
kev512 <kewin5@onet.eu>
2021-05-31 22:09:55 +0200

    Navigation Drawer - construction scheme

--- app/src/main/java/com/kewinplewa/androidportfolio/MainActivity.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/MainActivity.kt
@@ -3,13 +3,22 @@ package com.kewinplewa.androidportfolio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         Toast.makeText(this, "Witaj!", Toast.LENGTH_SHORT).show()
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
     }
 }
