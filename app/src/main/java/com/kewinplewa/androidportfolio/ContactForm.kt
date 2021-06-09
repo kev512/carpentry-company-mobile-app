c0023a9c39f1c640842c13fe0887149c05976d40
kev512 <kewin5@onet.eu>
2021-06-09 22:35:44 +0200

    Contact Form - draft

---
+++ app/src/main/java/com/kewinplewa/androidportfolio/ContactForm.kt
@@ -0,0 +1,11 @@
+package com.kewinplewa.androidportfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_form)
+    }
+}
