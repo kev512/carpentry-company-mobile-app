712525e002e25a4ae9db2d0d2be98e93a6b3de30
kev512 <kewin5@onet.eu>
2021-06-10 00:11:21 +0200

    Implementation of floating FAB Button - to contact form

--- app/src/main/java/com/kewinplewa/androidportfolio/About.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/About.kt
@@ -1,10 +1,15 @@
 package com.kewinplewa.androidportfolio
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.fragment_about.*
+import kotlinx.android.synthetic.main.fragment_oferta.*
 
 
 class About : Fragment() {
@@ -17,4 +22,15 @@ class About : Fragment() {
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        fab_btn.setOnClickListener {
+
+            val fieldIntent = Intent(this@About.context, ContactForm::class.java)
+            startActivity(fieldIntent)
+        }
+
+    }
 }
--- app/src/main/java/com/kewinplewa/androidportfolio/ContactForm.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/ContactForm.kt
@@ -1,5 +1,7 @@
 package com.kewinplewa.androidportfolio
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,18 @@ class ContactForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_form)
+
+        //support actionbar
+        val actionbar = supportActionBar
+        actionbar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#BE2222")))
+        actionbar!!.title = "Formularz Kontaktowy"
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
 }
