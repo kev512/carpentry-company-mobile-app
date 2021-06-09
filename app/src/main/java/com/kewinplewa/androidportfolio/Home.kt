6774c1a80f03117618f65a943ed3e5b3f004e1ea
kev512 <kewin5@onet.eu>
2021-06-09 20:55:26 +0200

    Additional navigation from Home Fragment to Offer Fragment

--- app/src/main/java/com/kewinplewa/androidportfolio/Home.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/Home.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 
 class Home : Fragment() {
@@ -15,6 +18,21 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<Button>(R.id.doOferta)
+        btn.setOnClickListener {
+            val ofertaFragment = Oferta()
+            val fragment: FragmentTransaction = fragmentManager!!.beginTransaction()
+            fragment.replace(R.id.fragment_container, Oferta())
+            fragment.commit()
+
+        }
+
+    }
 }
