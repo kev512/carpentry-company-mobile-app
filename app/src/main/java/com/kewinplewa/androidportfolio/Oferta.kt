ea6c1fe4d3b0ba276ae38109a69cc510feeb162e
kev512 <kewin5@onet.eu>
2021-06-09 18:15:24 +0200

    Offer Fragment - PDF Viewer wit company's offer implementation

--- app/src/main/java/com/kewinplewa/androidportfolio/Oferta.kt
+++ app/src/main/java/com/kewinplewa/androidportfolio/Oferta.kt
@@ -1,10 +1,13 @@
 package com.kewinplewa.androidportfolio
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.barteksc.pdfviewer.PDFView
+import kotlinx.android.synthetic.main.fragment_oferta.*
 
 
 class Oferta : Fragment() {
@@ -17,4 +20,16 @@ class Oferta : Fragment() {
         return inflater.inflate(R.layout.fragment_oferta, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_ofertaPDF.setOnClickListener {
+
+            val fieldIntent = Intent(this@Oferta.context, ViewPdfActivity::class.java)
+
+            fieldIntent.putExtra("ViewType", "assets")
+            startActivity(fieldIntent)
+        }
+
+    }
 }
